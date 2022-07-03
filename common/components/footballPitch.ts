7fb919ea72f300caf3e4865d76d1fb1ed7b6199d
Atle Magnussen <atlemagnussen@gmail.com>
2022-07-03 17:16:32 +0200

    jh

---
+++ common/components/footballPitch.ts
@@ -0,0 +1,29 @@
+import { html, css, LitElement } from "lit"
+import { customElement, property } from "lit/decorators.js"
+
+@customElement('football-pitch')
+export class FootballPitch extends LitElement {
+    static styles = css`
+        :host {
+            display: block;
+            border: solid 1px gray;
+            padding: 16px;
+            max-width: 800px;
+        }
+    `
+    
+
+    render() {
+        return html`
+            <canvas id="pitch">canvas?</canvas>
+        `
+    }
+
+    
+}
+
+declare global {
+    interface HTMLElementTagNameMap {
+        'my-element': MyElement
+    }
+}
